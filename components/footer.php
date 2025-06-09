73fd2296670a67c6d217e667c859ef2fd6bf7187
kashish <singhkashish364@.com>
2025-06-09 10:35:53 +0530

    change image

--- components/footer.php
+++ components/footer.php
@@ -4,7 +4,7 @@
             <!-- Left Section -->
             <div class="col-md-5 mb-4 mb-md-0">
                 <div class="d-flex align-items-center mb-4">
-                    <img src="assets/Logo/ctpslogo.png" 
+                    <img src="assets\about\ctps.png" 
                          alt="CT Logo" 
                             width="50" height="50"
                          class="footer-logo">
